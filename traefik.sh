84cbd8e97f7acdf647e9cbe8f69e9d5ec8e53f4d
Admin9705 <user@dunn.cloud>
2019-02-12 14:30:28 -0500

    more updates

--- traefik.sh
+++ traefik.sh
@@ -5,9 +5,63 @@
 # URL:        https://plexguide.com - http://github.plexguide.com
 # GNU:        General Public License v3.0
 ################################################################################
+source /opt/traefik/functions.sh
 
-main() {
-   local file=$1 val=$2 var=$3
-   [[ -e $file ]] || printf '%s\n' "$val" > "$file"
-   printf -v "$var" '%s' "$(<"$file")"
+traefikstart() {
+## Check for Traefik Running
+deployed=$(docker ps --format '{{.Names}}' | grep traefik)
+
+if [ "$deployed" == "traefik" ]; then
+  deployed="TRAEFIK Deployed"; else
+  deployed="TRAEFIK NOT Deployed"; fi
+
+# Menu Interface
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 Traefik - Reverse Proxy Interface Menu
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+[1] Top Level Domain App: [$tld]
+[2] Domain Provider     : [$provider]
+[3] Domain Name         : [$domain]
+[4] EMail Address       : [$email]
+[5] Deploy Traefik      : [$deployed]
+[6] Exit
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+# Standby
+read -p 'Type a Number | Press [ENTER]: ' typed < /dev/tty
+
+case $typed in
+    1 )
+      traefikstart ;;
+    2 )
+      traefikstart ;;
+    3 )
+
+      traefikstart ;;
+    4 )
+      traefikstart ;;
+    5 )
+
+      traefikstart ;;
+    z )
+      exit ;;
+    Z )
+      exit ;;
+    * )
+      primestart ;;
+esac
+
 }
+
+main /var/plexguide/traefik.provider NOT-SET provider
+main /var/plexguide/server.email NOT-SET email
+main /var/plexguide/server.domain NOT-SET domain
+main /var/plexguide/tld.program NOT-SET tld
+main /var/plexguide/traefik.deploy 'Not Deployed' deploy
+
+traefikmain
