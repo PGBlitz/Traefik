22fa86328b1c87fe105f82794c52c3b1e42684b9
Admin9705 <user@dunn.cloud>
2019-02-16 07:52:02 -0500

    update

--- functions.sh
+++ functions.sh
@@ -78,7 +78,9 @@ while read p; do
   let "pnum++"
   echo -n "$p"
   echo -n $(cat "/opt/traefik/providers/$provider/$p")
+  echo
 done </var/plexguide/prolist/prolist.sh
+  echo 
 
 echo
 typed2=999999999
